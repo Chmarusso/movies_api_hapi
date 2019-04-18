f50309a69203d9239ebb463c3a8f71190117bc40
Artur Ch <chmarus@gmail.com>
2019-04-18 15:20:00 +0200

    babel setup

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const Hapi = require('hapi')
+import Hapi from 'hapi'
+import models from './models'
 
 const init = async () => {
 
   const server = Hapi.server({
-      port: 3000,
-      host: 'localhost'
+    port: 3000,
+    host: 'localhost'
   })
 
   server.route({
-      method: 'GET',
-      path:'/',
-      handler: (request, h) => {
+    method: 'GET',
+    path:'/',
+    handler: (request, h) => {
 
-          return 'Hello World!'
-      }
+        return 'Hello World!'
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path:'/movies',
+    handler: async (request, h) => {
+      const movies = await models.Movie.findAll()
+
+      return movies
+    }
   })
 
   await server.start()
-  console.log('Server running on %ss', server.info.uri)
+  console.log('Server running on %s', server.info.uri)
 };
 
 process.on('unhandledRejection', (err) => {
